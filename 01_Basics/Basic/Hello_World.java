5f6fbf92c819a659fe2eda1c1f5797e6fea8e6ad
Ritik Wakchaure <rutikwakchaure805@gmail.com>
2025-09-21 13:44:14 +0530

    first commit

---
+++ 01_Basics/Basic/Hello_World.java
@@ -0,0 +1,9 @@
+public class Hello_World {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int sum = a + b;
+
+        System.out.println("Sum: " + sum);
+    }
+}
---
+++ 01_Basics/Funtions & Method/H.W/average_three_numbers.java
@@ -0,0 +1,10 @@
+public class Sum {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int sum = a + b;
+
+        System.out.println("Sum: " + sum);
+    }
+}
+
