19f265ae9adf0cf66602090cbc36265d58e265a8
Ritik Wakchaure <rutikwakchaure805@gmail.com>
2026-04-07 17:37:59 +0530

    Extra space use

--- TwoPointer/MoveZeroes.java
+++ TwoPointer/MoveZeroes.java
@@ -1,26 +1,41 @@
-public class MoveZeroes {
-    public static void moveZeroes(int[] nums) {
-        int lastNonZeroIndex = 0;
 
-        // Move all non-zero elements to the front
+public class MoveZeroes {
+
+    public static void moveZeroes(int[] nums) {
+
+        int[] result = new int[nums.length];
+        int pos = 0;
+
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                nums[lastNonZeroIndex++] = nums[i];
+                result[pos] = nums[i];
+                pos++;
+
             }
+
         }
 
-        // Fill the remaining positions with zeros
-        for (int i = lastNonZeroIndex; i < nums.length; i++) {
-            nums[i] = 0;
+        while (pos < nums.length) {
+            result[pos] = 0;
+            pos++;
         }
+
+        for (int i = 0; i < nums.length; i++) {
+
+            nums[i] = result[i];
+
+        }
+
     }
 
     public static void main(String[] args) {
-        int[] nums = {0, 1, 0, 3, 12};
+
+        int[] nums = { 0, 1, 0, 3, 12 };
 
         moveZeroes(nums);
 
         System.out.print("Array after moving zeroes: ");
+
         for (int num : nums) {
             System.out.print(num + " ");
         }
